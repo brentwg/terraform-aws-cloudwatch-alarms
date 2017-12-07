37bd688f087103104914db21ed77c60ed99f3a64
Brent Gawryluik <brent.gawryluik@scalar.ca>
2017-12-07 12:20:35 -0800

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+# ----------------
+# Cloudwatch Alarm
+# ----------------
+resource "aws_cloudwatch_metric_alarm" "this" {
+  alarm_name        = "${var.alarm_name}"
+  alarm_description = "${var.alarm_description}"
+
+  alarm_actions = ["${var.alarm_actions}"]
+
+  metric_name         = "${var.metric_name}"
+  namespace           = "${var.namespace}"
+  comparison_operator = "${var.comparison_operator}"
+  statistic           = "${var.statistic}"
+  threshold           = "${var.threshold}"
+  period              = "${var.period}"
+  evaluation_periods  = "${var.evaluation_periods}"
+  treat_missing_data  = "${var.treat_missing_data}"
+
+  dimensions = "${var.dimensions}"
+}
